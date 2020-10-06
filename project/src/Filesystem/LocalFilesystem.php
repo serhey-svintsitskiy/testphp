163a5b27293bddddf10e11e871af76e8ce1a6bd9
serhey-svintsitskiy <sergeys08.sv@gmail.com>
2020-10-07 00:29:10 +0300

    test code

--- project/src/Filesystem/LocalFilesystem.php
+++ project/src/Filesystem/LocalFilesystem.php
@@ -3,8 +3,23 @@
 
 namespace Filesystem;
 
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local;
+
 
 class LocalFilesystem
 {
+    private string $rootDir;
+    
+    public function __construct(string $rootDir)
+    {
+        $this->rootDir = $rootDir;
+    }
 
+    public function test()
+    {
+        $adapter = new Local($this->rootDir . '/../var');
+        $filesystem = new Filesystem($adapter);
+        print_r($filesystem->listContents());
+    }
 }
--- project/web/index.php
+++ project/web/index.php
@@ -1,3 +1,6 @@
 <?php
 
-phpinfo();
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$test = new \Filesystem\LocalFilesystem(__DIR__);
+$test->test(); 
